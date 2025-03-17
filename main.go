e70092dcba61f3f459acd3f072db6b68e52a1c4b
mnorrsken <martin.norrsken@gmail.com>
2025-03-18 00:34:23 +0100

    Lägg till loggning av serverstart och begärningar i main.go samt hantera fel vid mallutförande

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -9,15 +10,27 @@ var tmpl = template.Must(template.ParseFiles("templates/index.html"))
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	log.Printf("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Received request from %s", r.RemoteAddr)
+	log.Printf("Request method: %s, URL path: %s", r.Method, r.URL.Path)
+	log.Printf("User agent: %s", r.UserAgent())
+
 	clientIP := r.RemoteAddr
 	xForwardedFor := r.Header.Get("X-Forwarded-For")
 	xForwardedHost := r.Header.Get("X-Forwarded-Host")
 	xForwardedProto := r.Header.Get("X-Forwarded-Proto")
 	host := r.Host
+
+	log.Printf("Client IP: %s", clientIP)
+	log.Printf("X-Forwarded-For: %s", xForwardedFor)
+	log.Printf("X-Forwarded-Host: %s", xForwardedHost)
+	log.Printf("X-Forwarded-Proto: %s", xForwardedProto)
+	log.Printf("Host: %s", host)
 
 	data := struct {
 		ClientIP        string
@@ -33,5 +46,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		Host:            host,
 	}
 
-	tmpl.Execute(w, data)
+	if err := tmpl.Execute(w, data); err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Printf("Request handled successfully")
 }
